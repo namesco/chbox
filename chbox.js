cdecb52a063740ecf7849dd8f4d8afc8d48dc4d4
James Buncle <jbuncle@names.co.uk>
2017-08-11 15:29:34 +0100

    Rewrote selection handler for better compatability with Roundcube 1.3

--- chbox.js
+++ chbox.js
@@ -46,35 +46,24 @@ function chbox_menu(){
 }
 
 if (window.rcmail) {
-  rcmail.addEventListener('init', function(evt) {
+  rcmail.addEventListener('init', function (evt) {
     rcmail.register_command('plugin.chbox.selectmenu', rcmail_selectmenu, true);
-    // add event-listener to message list
-    if (rcmail.message_list) {
-      rcmail.message_list.addEventListener('select', function(list){
-        $('#messagelist input:checked').removeAttr('checked');
-        var selection = rcmail.message_list ? $.merge([], rcmail.message_list.get_selection()) : [];
-        // exit if no mailbox specified or if selection is empty
-        if (!rcmail.env.uid && !selection.length)
-          return;
-        // also select childs of collapsed rows
-        /*
-        for (var uid, i=0, len=selection.length; i<len; i++) {
-          uid = selection[i];
-          if (rcmail.message_list.rows[uid].has_children && !rcmail.message_list.rows[uid].expanded)
-            rcmail.message_list.select_childs(uid);
-        }
-        var selection = rcmail.message_list ? $.merge([], rcmail.message_list.get_selection()) : [];
-        */
-        for (var uid, i=0, len=selection.length; i<len; i++) {
-            uid = selection[i];
-            var select = document.getElementById('rcmselect'+uid);
-            if (select) {
-				select.checked = true;
-				//rcmail.message_list.select_childs(uid);
-			}
-        }
+      // add event-listener to message list
+      if (rcmail.message_list) {
+        rcmail.message_list.addEventListener('select', function (list) {
+
+        var selection = list.selection;
+        var rows = list.rows;
+				
+        $.each(rows, function(index, row){
+          var uid = row.uid;
+          var select = document.getElementById('rcmselect' + uid);
+          if (select) {
+            select.checked = chbox_in_array(uid, selection);
+          }
+        });
       });
-    }
+   }
   });
 
   rcmail.addEventListener('listupdate','chbox_menu');
